91b65c5e96bbf05f699e58fa6488a1c96b86b1a6
ldong1204 <1191217575@qq.com>
2020-12-17 21:02:03 +0800

    创建分支dev

--- HelloGit/src/main/java/com/ldong/git/HelloGit.java
+++ HelloGit/src/main/java/com/ldong/git/HelloGit.java
@@ -12,6 +12,7 @@ import java.sql.SQLOutput;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("HelloGit");
+        System.out.println("创建分支");
     }
 
 }
