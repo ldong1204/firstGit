ce13852cd66a2d1e4ba67551dee443f5db0e700f
ldong1204 <1191217575@qq.com>
2020-12-17 20:20:16 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/ldong/git/HelloGit.java
@@ -0,0 +1,17 @@
+package com.ldong.git;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author ldong
+ * @fuction
+ * @Email yangdonghua321@gmail.com
+ * @number 15507642660
+ * @create 2020-12-17 20:02
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+    }
+
+}
